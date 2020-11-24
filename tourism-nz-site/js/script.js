10a64828145303fbded84d719b0e27a40ff72236
Joshvdw <Joshvdw@live.com>
2020-11-24 16:09:19 +1300

    started coding

---
+++ tourism-nz-site/js/script.js
@@ -0,0 +1,3 @@
+console.log(key); //key comes from external file mapKey.js
+var script = '<script src="https://maps.googleapis.com/maps/api/js?key=' + key + '&callback=initMap&libraries=&v=weekly" defer></script>'
+console.log (script);
